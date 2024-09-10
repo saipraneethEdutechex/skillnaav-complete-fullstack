552345bc96d6a5338b07e71fa51a38db19f7f3f6
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-09-10 14:45:10 +0530

    IP

--- comp-frontend/src/setupProxy.js
+++ comp-frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api2", // Route for the second proxy
     createProxyMiddleware({
-      target: "http://localhost:5100", // Target server for the second proxy
+      target: "http://35.95.38.233:5100", // Target server for the second proxy
       changeOrigin: true,
       pathRewrite: { "^/api2": "" }, // Optionally rewrite the path
     })
